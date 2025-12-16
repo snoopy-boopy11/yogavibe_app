91625d84849f71ffb313beff0dbad4c5f77de2a9
Алиса Шурман <sleepy13@MacBook-Air-Alisa.local>
2025-12-17 00:14:05 +0300

    fixes in config file

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,7 +4,7 @@ from typing import Optional
 
 class Settings(BaseSettings):
     # Настройки JWT
-    SECRET_KEY: str = "your-secret-key-here-change-in-production"
+    SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
